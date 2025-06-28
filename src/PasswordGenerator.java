a21282a9fec2506e3799bffe2898b426b6a1e32d
fratur <francescoturco2795@gmail.com>
2025-06-28 16:49:22 +0200

    generazione-password

--- src/PasswordGenerator.java
+++ src/PasswordGenerator.java
@@ -9,26 +9,31 @@ public class PasswordGenerator {
         
         Scanner scan = new Scanner(System.in);
 
-        System.out.print("Inserisci il tuo nome: ");
+        //Dati Utente
+        System.out.print("Inserisci il tuo nome:");
         String nome = scan.nextLine();
 
-        System.out.print("Inserisci il tuo cognome: ");
+        System.out.print("Inserisci il tuo cognome:");
         String cognome = scan.nextLine();
 
-        System.out.print("Inserisci il tuo colore preferito: ");
+        System.out.print("Inserisci il tuo colore preferito:");
         String colorePreferito = scan.nextLine();
 
-        System.out.print("Inserisci il giorno di nascita (numero): ");
+        System.out.print("Inserisci il giorno di nascita (numero):");
         int giorno = scan.nextInt();
 
-        System.out.print("Inserisci il mese di nascita (numero): ");
+        System.out.print("Inserisci il mese di nascita (numero):");
         int mese = scan.nextInt();
 
-        System.out.print("Inserisci l'anno di nascita: ");
+        System.out.print("Inserisci l'anno di nascita:");
         int anno = scan.nextInt();
 
         // Calcolo somma
         int sommaData = giorno + mese + anno;
+        
+        //Generazione Password
+        String password = nome + "-" + cognome + "-" + colorePreferito + "-" + sommaData;
+        System.out.println("La tua password sicurissima è:" + password);
 
        
 
